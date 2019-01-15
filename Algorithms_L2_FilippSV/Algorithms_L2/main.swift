a3c8a15ebb011095641b6e60bf5d20bcbbd312a8
FeeLeePPy4 <FLP4S@ya.ru>
2019-01-16 01:04:07 +0300

    some changes

---
+++ Algorithms_L2_FilippSV/Algorithms_L2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Algorithms_L2
+//
+//  Created by Filipp S.V. on 16/01/2019.
+//  Copyright © 2019 Filipp S.V. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
