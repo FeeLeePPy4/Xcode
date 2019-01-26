59d45c9e21051dfc03af0d68105cb597d0442c18
FeeLeePPy4 <FLP4S@ya.ru>
2019-01-26 23:43:42 +0300

    add L2

---
+++ Algorithms_HW/L2_FilippStolyarov/L2_FilippStolyarov/main.swift
@@ -0,0 +1,67 @@
+//
+//  Algorithms_L2
+//
+//  Created by Filipp S.V. on 16/01/2019.
+//
+
+import Foundation
+
+// 1) Перевод из десятичной в двоичную СС.
+
+//var numberRound = Int()
+//var arr = [Int]()
+//var i = 1
+//
+//func myfunc (d: Double) -> Double {
+//    guard i < 12 else {return 0}
+//    i += 1
+//    var dInp = d
+//    dInp *= 2
+//    if dInp >= 1 {
+//        arr.append(1)
+//        numberRound = Int(dInp)
+//        dInp = dInp - Double(numberRound)
+//        return myfunc(d: dInp)
+//    }else {
+//        arr.append(Int(dInp))
+//        return myfunc(d: dInp)
+//    }
+//}
+//
+//myfunc(d: 0.99)
+//print("0.\(arr)")
+
+// 2) Возведение в степень.
+
+//a)
+
+//var a = 2
+//var b = 10
+//var c = a
+//
+//for _ in 1..<b {
+//    c *= a
+//    print(c)
+//}
+
+//b)
+
+//var cF = Int()
+//func myfunc (aF: Int, bF: Int) -> Int {
+//    guard bF > 1 else {return 404}
+//    cF = aF
+//    cF += aF
+//    print("\(cF)  \(bF)")
+//    return myfunc(aF: cF, bF: bF - 1)
+//}
+//
+//myfunc(aF: 2, bF: 10)
+
+    
+
+    
+    
+
+
+
+
