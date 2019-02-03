470e1f72d8f4290560b521dc398dca605a5c9b05
FeeLeePPy4 <FLP4S@ya.ru>
2019-02-04 00:43:37 +0300

    HW_2)&3)_done

--- Algorithms_HW/L7_FilippStolyarov/L7_FilippStolyarov/main.swift
+++ Algorithms_HW/L7_FilippStolyarov/L7_FilippStolyarov/main.swift
@@ -5,7 +5,8 @@
 //
 
 import Foundation
-// 3) G in width
+
+// 2) G in depth
 /*
 var matrix = [[0,1,1,1,0,0,0,0],
               [1,0,1,0,1,1,0,0],
@@ -17,22 +18,58 @@ var matrix = [[0,1,1,1,0,0,0,0],
               [0,0,0,0,1,1,1,0]]
 
 var arrMark = [0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0]  // top : mark(0 - not read, 1 - read, 2 - done)
-var queue = [Int]()
+var stack = [Int]()
 
-queue.append(0)  // start from top 0.
-
-repeat {
+func GinDepth(top: Int) {
     var index = 0
-    for i in matrix[queue[0]] {
+    let stackTop = stack.last!
+    stack.removeLast()
+    for i in matrix[stackTop] {
         if i == 1 && arrMark[index] == 0 {
-            queue.append(index)
             arrMark[index] = 1
+            stack.append(index)
         }
         index += 1
     }
-    arrMark[queue[0]] = 2
-    queue.removeFirst()
-} while !queue.isEmpty
+    
+    arrMark[stackTop] = 2
+    if !stack.isEmpty { GinDepth(top: stack.last!) }
+}
+
+stack.append(0)  // start from top 0
+GinDepth(top: 0)
 
 print(arrMark)  // all tops must be 2
 */
+
+// 3) G in width
+/*
+ var matrix = [[0,1,1,1,0,0,0,0],
+ [1,0,1,0,1,1,0,0],
+ [1,1,0,1,1,0,0,0],
+ [1,0,1,0,0,0,1,0],
+ [0,1,1,0,0,1,0,1],
+ [0,1,0,0,1,0,0,1],
+ [0,0,0,1,0,0,0,1],
+ [0,0,0,0,1,1,1,0]]
+ 
+ var arrMark = [0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0]  // top : mark(0 - not read, 1 - read, 2 - done)
+ var queue = [Int]()
+ 
+ queue.append(0)  // start from top 0.
+ 
+ repeat {
+ var index = 0
+ for i in matrix[queue[0]] {
+ if i == 1 && arrMark[index] == 0 {
+ queue.append(index)
+ arrMark[index] = 1
+ }
+ index += 1
+ }
+ arrMark[queue[0]] = 2
+ queue.removeFirst()
+ } while !queue.isEmpty
+ 
+ print(arrMark)  // all tops must be 2
+ */
