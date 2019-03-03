d9a47cc10d1bae0288f8d455d91be8dc800fa519
FeeLeePPy4 <FLP4S@ya.ru>
2019-03-03 19:50:20 +0300

    Revert "Revert "111""
    This reverts commit fbd72ce3447d37a8ec3c107a25568945c65f90b2.

---
+++ UI/social_network/social_network/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  social_network
+//
+//  Created by Filipp S.V. on 03/03/2019.
+//  Copyright © 2019 FilippStolyarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
